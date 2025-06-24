7da022606acfb59310b778027cfb00613a023a4b
PatrickSutton1 <patricksutton2301@gmail.com>
2025-06-24 15:02:31 +1000

    Initial attempt - wrong

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+import sys
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+from google import genai
+from google.genai import types
+
+client = genai.Client(api_key=api_key)
+
+user_prompt = sys.argv[1:]
+prompt_history = ["User Input"]
+prompt_history = prompt_history.extend(user_prompt)
+
+messages = [
+    types.Content(role="user",parts=[types.Part(text=user_prompt)]),
+]
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit(1)
+    else:
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', 
+            contents=messages,
+        )
+        print(response.text)
+
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+ 
+
+if __name__ == "__main__":
+    main()
